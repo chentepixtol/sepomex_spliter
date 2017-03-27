3572a854043b67ba9a7593c8a49e9ba0d46e83df
Vicente Mendoza <vicente.mendoza@linio.com>
2017-03-27 13:15:34 -0600

    add FileDumper

---
+++ app/models/file_dumper.rb
@@ -0,0 +1,14 @@
+class FileDumper
+
+    def initialize(name)
+        @filename = "data/" + name.gsub(/\s/, '_') + ".txt"
+        File.new(@filename, "w") if !File.exist?(@filename)
+    end
+
+    def dump(row)
+        File.open(@filename, "a") do |file|
+            file.puts row.values.join("|")
+        end
+    end
+
+end
