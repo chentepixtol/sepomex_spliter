e1472057c0bf6e27d6d59b087d7b40a5cbc091ab
Vicente Mendoza <vicente.mendoza@linio.com>
2017-03-27 06:50:30 -0600

    initial commit

---
+++ test/models/encryption_test.rb
@@ -0,0 +1,20 @@
+class EncryptionTest < ActiveSupport::TestCase
+    test "text should be encrypted" do
+        encrypter = Encryption.new(
+            "config/certificates/private.pem",
+            "config/certificates/public.pem",
+            "chentux"
+        )
+        assert encrypter.encrypt("Encrypt this!").bytesize() > 0
+    end
+
+    test "text should be encrypted and then decrypted" do
+        encrypter = Encryption.new(
+            "config/certificates/private.pem",
+            "config/certificates/public.pem",
+            "chentux"
+        )
+        encrypted = encrypter.encrypt("Encrypt this!")
+        assert_equal "Encrypt this!", encrypter.decrypt(encrypted)
+    end
+end
